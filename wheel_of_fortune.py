3344ccc01fa534d1163663c90f24124ec448570b
charlierehder <charlesreh1577@gmail.com>
2022-04-08 14:33:18 -0500

    implement spin wheel function

--- wheel_of_fortune.py
+++ wheel_of_fortune.py
@@ -3,6 +3,26 @@ import random
 def getWord():
     with open('words.txt', 'r') as f:
         return random.choice(f.readlines()).strip()
+def spinWheel():
+    rand_val = random.random()
+    if rand_val < 0.29:
+        return 500
+    elif rand_val >= 0.29 and rand_val < 0.46:
+        return 600
+    elif rand_val >= 0.46 and rand_val < 0.58:
+        return 650
+    elif rand_val >= 0.58 and rand_val < 0.71:
+        return 700
+    elif rand_val >= 0.71 and rand_val < 0.75:
+        return 800
+    elif rand_val >= 0.75 and rand_val < 0.79:
+        return 900
+    elif rand_val >= 0.79 and rand_val < 0.83:
+        return 5000
+    elif rand_val >= 0.83 and rand_val < 0.91:
+        return -1 # BANKRUPT
+    else:
+        return -2 # LOSE A TURN
 
 for i in range(1,4): # ITERATE THROUGH ROUND 1-3
    
@@ -26,7 +46,7 @@ for i in range(1,4): # ITERATE THROUGH ROUND 1-3
                 print("Please give a valid input")
             else:
                 if option_num == 1:
-                    print("SpinWheel()")
+                    print(spinWheel())
                 elif option_num == 2:
                     print("BuyAVowel()")
                 elif option_num == 3:
